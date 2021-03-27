2b25df5ba8a99b9a41b597bda4d2a7082988e853
Jacob Bare <jacob@parameter1.com>
2021-03-27 15:16:02 -0500

    Add email from and app url env vars

--- api/src/env.js
+++ api/src/env.js
@@ -5,6 +5,8 @@ const {
 } = require('envalid');
 
 module.exports = cleanEnv(process.env, {
+  APP_URL: str({ desc: 'The user-facing, application URL.' }),
+  EMAIL_FROM: str({ desc: 'The from name to use when sending notification emails.', default: 'Ticket Scores <support@ticketscores.com>' }),
   MONGO_URI: str({ desc: 'The MongoDB instance to connect to.' }),
   HOST: str({ desc: 'The host that the service will run on.', default: '0.0.0.0' }),
   PORT: port({ desc: 'The port that the service will run on.', default: 80 }),
