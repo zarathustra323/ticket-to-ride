c1368ac9ea07675546a37e689b368f036e1eb05a
Jacob Bare <jacob@parameter1.com>
2021-03-27 16:40:23 -0500

    Add apollo link context

--- app/src/apollo/client.js
+++ app/src/apollo/client.js
@@ -1,13 +1,20 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 import { name, version } from '../../package.json';
 
 const { VUE_APP_GRAPHQL_URL } = process.env;
+
+const httpLink = createHttpLink({ uri: VUE_APP_GRAPHQL_URL });
+const contextLink = setContext(() => {
+  const authToken = localStorage.getItem('authToken');
+  return { headers: { ...(authToken && { authorization: `Bearer ${authToken}` }) } };
+});
 
 export default new ApolloClient({
   name,
   version,
   cache: new InMemoryCache(),
-  link: createHttpLink({ uri: VUE_APP_GRAPHQL_URL }),
+  link: contextLink.concat(httpLink),
 });
