d756d493f5c0c71ea2b672c9365dde4c5a4b74c8
Jacob Bare <jacob@parameter1.com>
2021-04-04 12:38:43 -0500

    Create basic select component

---
+++ app/src/components/forms/select.vue
@@ -0,0 +1,29 @@
+<template>
+  <select
+    :class="classes"
+    @change="$emit('change', $event.target.value)"
+  >
+    <slot />
+  </select>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    classes: [
+      'block',
+      'w-full',
+      'pl-3',
+      'pr-10',
+      'py-2',
+      'text-base',
+      'border-gray-300',
+      'focus:outline-none',
+      'focus:ring-blue-500',
+      'focus:border-blue-500',
+      'sm:text-sm',
+      'rounded-md',
+    ],
+  }),
+};
+</script>
